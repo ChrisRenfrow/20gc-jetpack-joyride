2114bf9cb978634a2fa17e51e5c4f7d440b113b7
Chris Renfrow <dev@chrisrenfrow.me>
2025-02-10 21:08:05 -0800

    Refactor segment_manager
    Uses a better(?) weighted choice system, which should result in segments appearing at more expected frequencies.
    Also exchanges the active segments concept with a single active segment and queued segment system which allows us to decide when to spawn a segment rather than hoping for the right conditions.

--- assets/systems/segment_manager/segment_manager.gd
+++ assets/systems/segment_manager/segment_manager.gd
@@ -1,20 +1,18 @@
 class_name SegmentManager extends Node2D
 
-## Maximum missiles per 100m
-const MISSILE_MAX_FREQ := 1
-
-enum State {
-	## Default behavior, non-exclusive
-	DEFAULT,
-	## Lasers are on-screen, exclusive
+enum SegmentType {
+	ZAPPER,
 	LASER,
+	MISSILE,
+	COIN,
 }
 
-@export var zapper_probability: float = 0.8
-@export var laser_probability: float = 0.2
-# Missiles can appear any time zappers (or coins) are on screen, so their probability needs to be pretty low
-@export var missile_probability: float = 0.02
-#@export var coin_probability: float = 0.4
+@export var segment_weights := {
+	SegmentType.ZAPPER: 5.0,
+	SegmentType.LASER: 2.0,
+	SegmentType.MISSILE: 2.0,
+	SegmentType.COIN: 1.0
+}
 
 var scroll_speed: float:
 	get: return Globals.scroll_speed
@@ -22,39 +20,71 @@ var scroll_speed: float:
 var distance: float:
 	get: return Globals.distance
 
-
-var _state := State.DEFAULT
 var _segments := {
-	"zappers": [] as Array[PackedScene],
-	"lasers": [] as Array[PackedScene],
-	"missiles": [] as Array[PackedScene]
+	SegmentType.ZAPPER: [] as Array[PackedScene],
+	SegmentType.LASER: [] as Array[PackedScene],
+	SegmentType.MISSILE: [] as Array[PackedScene],
+	SegmentType.COIN: [] as Array[PackedScene],
 }
 
-var _active_segments := {
-	"zappers": [] as Array[BaseSegment],
-	"lasers": [] as Array[BaseSegment],
-	"missiles": [] as Array[BaseSegment]
-}
+var _active_segment: BaseSegment
+var _active_segment_type: SegmentType
 
-var _num_active_segments: int = 0:
-	get: return _active_segments.keys().reduce(
-			func(c, k): return c + _active_segments.get(k).size(),
-			0
-		)
+var _active_overlay_segment: BaseSegment
+# Can only be MISSILE, for now?
+var _active_overlay_segment_type: SegmentType
+
+var _queued_segment: BaseSegment
+var _queued_segment_type: SegmentType
+
+var _weighted_segments: Dictionary:
+	get: return segment_weights
 
 func _ready() -> void:
 	for segment_type in _segments.keys():
-		_segments[segment_type] = _load_segments_from_path("res://assets/segments/" + segment_type)
-		prints("Loaded", _segments[segment_type].size(), segment_type, "segments")
+		var type_name = SegmentType.find_key(segment_type).to_lower() + "s"
+		_segments[segment_type] = _load_segments_from_path("res://assets/segments/" + type_name)
+		prints("Loaded", _segments[segment_type].size(), type_name, "segments")
+
+	_queue_random_segment()
 
 func _process(_delta: float) -> void:
-	_clean_inactive_segments()
+	_try_spawn_queued_segment()
 
-	match _state:
-		State.DEFAULT:
-			_handle_default_state()
-		State.LASER:
-			_handle_laser_state()
+func _queue_random_segment() -> void:
+	print("Queuing random segment:")
+	var picked_type = _weighted_choice(_weighted_segments)
+	print("Picked type: ", SegmentType.find_key(picked_type))
+	_queued_segment = _get_random_segment_of_type(picked_type)
+	_queued_segment_type = picked_type
+	print("Selected segment ", _queued_segment.name)
+
+func _get_random_segment_of_type(type: SegmentType) -> BaseSegment:
+	return _segments[type].pick_random().instantiate()
+
+func _try_spawn_queued_segment() -> void:
+	var active_segment_done = not is_instance_valid(_active_segment)
+	var active_overlay_segment_done = not is_instance_valid(_active_overlay_segment)
+	match [_queued_segment_type, active_segment_done]:
+		[SegmentType.MISSILE, _] when active_overlay_segment_done:
+			if _active_segment_type != SegmentType.LASER or _active_segment_type == SegmentType.LASER and active_segment_done:
+				_spawn_queued_segment()
+		[SegmentType.LASER, true] when active_overlay_segment_done:
+			_spawn_queued_segment()
+		[SegmentType.ZAPPER, true], [SegmentType.COIN, true]:
+			_queued_segment.global_position.x = get_viewport_rect().size.x + 10
+			_spawn_queued_segment()
+
+func _spawn_queued_segment() -> void:
+	if _queued_segment_type == SegmentType.MISSILE:
+		_active_overlay_segment = _queued_segment
+		_active_overlay_segment_type = SegmentType.MISSILE
+	else:
+		_active_segment = _queued_segment
+		_active_segment_type = _queued_segment_type
+
+	add_child(_queued_segment)
+	_queue_random_segment()
 
 func _load_segments_from_path(path: String) -> Array[PackedScene]:
 	var segments: Array[PackedScene] = []
@@ -70,18 +100,14 @@ func _load_segments_from_path(path: String) -> Array[PackedScene]:
 		if dir.current_is_dir() and not file_name.begins_with("."):
 			segments.append_array(_load_segments_from_path(path + "/" + file_name))
 		elif file_name.ends_with(".tscn"):
-			var scene = load(path + "/" + file_name) as PackedScene
+			var full_path = path + "/" + file_name
+			var scene = load(full_path) as PackedScene
 			if not scene:
-				push_error("Failed to load scene: " + path + "/" + file_name)
-				continue
-
-			var instance = scene.instantiate()
-			if not instance is BaseSegment:
-				push_error("Scene is not a BaseSegment: " + path + "/" + file_name)
-				instance.free()
-				continue
-
-			segments.append(scene)
+				push_error("Failed to load scene: " + full_path)
+			elif not scene.instantiate() is BaseSegment:
+				push_error("Scene is not a BaseSegment: " + full_path)
+			else:
+				segments.append(scene)
 		file_name = dir.get_next()
 
 	dir.list_dir_end()
@@ -91,45 +117,18 @@ func _load_segments_from_path(path: String) -> Array[PackedScene]:
 
 	return segments
 
-func _clean_inactive_segments() -> void:
-	for segment_type in _active_segments.keys():
-		var new_active_segments = _active_segments.get(segment_type).filter(
-				func(segment): return is_instance_valid(segment)
-			)
-		_active_segments[segment_type] = new_active_segments
 
-func _handle_default_state() -> void:
-	if _try_spawn_laser():
-		return
+# Probably should go in a utility file or something
+## choices should be keyed with the option, with the value being a float for the weight
+func _weighted_choice(choices: Dictionary) -> Variant:
+	var sum_weight: float = 0.0
+	for option in choices.keys():
+		sum_weight += choices[option]
 
-	_try_spawn_zapper()
-	_try_spawn_missile()
-
-func _handle_laser_state() -> void:
-	if _active_segments["lasers"].is_empty():
-		_state = State.DEFAULT
-
-func _try_spawn_laser() -> bool:
-	if randf() < laser_probability and not _segments.lasers.is_empty() and _num_active_segments == 0:
-		_spawn_segment("lasers")
-		_state = State.LASER
-		return true
-	return false
-
-func _try_spawn_zapper() -> void:
-	if randf() < zapper_probability and not _segments.zappers.is_empty() and _num_active_segments == 0:
-		var segment = _spawn_segment("zappers")
-		segment.global_position.x = get_viewport_rect().size.x
-
-func _try_spawn_missile() -> void:
-	if _active_segments["missiles"].size() > 0:
-		return
-
-	if randf() < missile_probability and not _segments.missiles.is_empty():
-		_spawn_segment("missiles")
-
-func _spawn_segment(type: String) -> BaseSegment:
-	var segment = _segments[type].pick_random().instantiate()
-	add_child(segment)
-	_active_segments[type].append(segment)
-	return segment
+	var roll = randf_range(0.0, sum_weight)
+	var cursor: float = 0
+	for option in choices.keys():
+		cursor += choices[option]
+		if cursor >= roll:
+			return option
+	return
