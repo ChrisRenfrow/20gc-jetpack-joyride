070a2328de0e25dfabd1c301fd57394a78ba00f7
Chris Renfrow <dev@chrisrenfrow.me>
2025-03-09 10:48:43 -0700

    Decouple missile segment spawning logic from other segments
    Also switches to a random cooldown timer for more consistent and controlled spawning behavior.

--- assets/systems/segment_manager/segment_manager.gd
+++ assets/systems/segment_manager/segment_manager.gd
@@ -7,12 +7,17 @@ enum SegmentType {
 	COIN,
 }
 
+## Weighted distribution of segments by type (excluding missiles)
 @export var segment_weights := {
 	SegmentType.ZAPPER: 5.0,
 	SegmentType.LASER: 2.0,
-	SegmentType.MISSILE: 2.0,
 	SegmentType.COIN: 1.0
 }
+
+## The minimum number of seconds to wait to queue a missile segment
+@export var missile_min_cooldown := 6.0;
+## The maximum number of seconds to wait to queue a missile segment
+@export var missile_max_cooldown := 48.0;
 
 var scroll_speed: float:
 	get: return Globals.scroll_speed
@@ -30,9 +35,9 @@ var _segments := {
 var _active_segment: BaseSegment
 var _active_segment_type: SegmentType
 
-var _active_overlay_segment: BaseSegment
-# Can only be MISSILE, for now?
-var _active_overlay_segment_type: SegmentType
+var _active_missile_segment: BaseSegment
+var _queued_missile_segment: BaseSegment
+var _missile_cooldown_timer: Timer
 
 var _queued_segment: BaseSegment
 var _queued_segment_type: SegmentType
@@ -40,16 +45,22 @@ var _queued_segment_type: SegmentType
 var _weighted_segments: Dictionary:
 	get: return segment_weights
 
+var _game_active := false:
+	get: return Globals.game_state == Globals.GameState.ACTIVE
+
 func _ready() -> void:
 	for segment_type in _segments.keys():
 		var type_name = SegmentType.find_key(segment_type).to_lower() + "s"
 		_segments[segment_type] = _load_segments_from_path("res://assets/segments/" + type_name)
 		prints("Loaded", _segments[segment_type].size(), type_name, "segments")
 
+	_missile_timer_setup()
 	_queue_random_segment()
 
 func _process(_delta: float) -> void:
-	_try_spawn_queued_segment()
+	if _game_active:
+		_try_spawn_queued_segment()
+		_try_spawn_queued_missile_segment()
 
 func _queue_random_segment() -> void:
 	print("Queuing random segment:")
@@ -64,27 +75,44 @@ func _get_random_segment_of_type(type: SegmentType) -> BaseSegment:
 
 func _try_spawn_queued_segment() -> void:
 	var active_segment_done = not is_instance_valid(_active_segment)
-	var active_overlay_segment_done = not is_instance_valid(_active_overlay_segment)
+	var missile_segment_active = is_instance_valid(_active_missile_segment)
 	match [_queued_segment_type, active_segment_done]:
-		[SegmentType.MISSILE, _] when active_overlay_segment_done:
-			if _active_segment_type != SegmentType.LASER or _active_segment_type == SegmentType.LASER and active_segment_done:
-				_spawn_queued_segment()
-		[SegmentType.LASER, true] when active_overlay_segment_done:
+		[SegmentType.LASER, true] when not missile_segment_active:
 			_spawn_queued_segment()
 		[SegmentType.ZAPPER, true], [SegmentType.COIN, true]:
 			_queued_segment.global_position.x = get_viewport_rect().size.x + 10
 			_spawn_queued_segment()
 
 func _spawn_queued_segment() -> void:
-	if _queued_segment_type == SegmentType.MISSILE:
-		_active_overlay_segment = _queued_segment
-		_active_overlay_segment_type = SegmentType.MISSILE
-	else:
-		_active_segment = _queued_segment
-		_active_segment_type = _queued_segment_type
-
+	print("Spawning queued segment: ", _queued_segment.name)
 	add_child(_queued_segment)
+	_active_segment = _queued_segment
+	_active_segment_type = _queued_segment_type
 	_queue_random_segment()
+
+func _missile_timer_setup() -> void:
+	_missile_cooldown_timer = Timer.new()
+	_missile_cooldown_timer.one_shot = true
+	_missile_cooldown_timer.autostart = true
+	_missile_cooldown_timer.wait_time = randf_range(missile_min_cooldown, missile_max_cooldown)
+	add_child(_missile_cooldown_timer)
+
+func _try_spawn_queued_missile_segment() -> void:
+	var missile_segment_queued = is_instance_valid(_queued_missile_segment)
+	var missile_segment_active = is_instance_valid(_active_missile_segment)
+	if _active_segment_type != SegmentType.LASER and \
+		not missile_segment_active and missile_segment_queued:
+			_spawn_queued_missile_segment()
+	if _missile_cooldown_timer.is_stopped() and not missile_segment_queued:
+		_queued_missile_segment = _get_random_segment_of_type(SegmentType.MISSILE)
+
+func _spawn_queued_missile_segment() -> void:
+	print("Spawning queued missile segment: ", _queued_missile_segment.name)
+	_active_missile_segment = _queued_missile_segment
+	add_child(_queued_missile_segment)
+	_queued_missile_segment = null
+	_missile_cooldown_timer.wait_time = randf_range(missile_min_cooldown, missile_max_cooldown)
+	_missile_cooldown_timer.start()
 
 func _load_segments_from_path(path: String) -> Array[PackedScene]:
 	var segments: Array[PackedScene] = []
