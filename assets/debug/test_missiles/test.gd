b81afd7c3d51b37c88f9a0b3f6dccd3a7bd3d283
Chris Renfrow <dev@chrisrenfrow.me>
2025-03-10 11:47:29 -0700

    Auto-migrate to 4.4

--- assets/debug/test_missiles/test.gd
+++ assets/debug/test_missiles/test.gd
@@ -7,7 +7,7 @@ var screen_size: Vector2
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-	missile_spawn_timer.wait_time = 2
+	missile_spawn_timer.wait_time = 1
 	missile_spawn_timer.autostart = true
 	missile_spawn_timer.timeout.connect(_on_missile_spawn_timer_timeout)
 	add_child(missile_spawn_timer)
@@ -15,9 +15,8 @@ func _ready() -> void:
 func _on_missile_spawn_timer_timeout() -> void:
 	var missile = missile_scene.instantiate()
 
-	missile.homing = randf() > 0.5
+	missile.homing = true
 	missile.position.y = randf_range(10, screen_size.y - 10)
-	missile.speed = randf_range(500, 1000)
-	missile.show_homing_vector = true
+	missile.show_homing_vector = false
 
 	add_child(missile)
